4d66e1e0e2073d65371db988019dacbb4d25c693
Ryan Rushton <ryanr@canva.com>
2021-10-25 09:33:38 +1100

    Clean up unused imports.

--- src/main/kotlin/com/dprint/services/FormatterService.kt
+++ src/main/kotlin/com/dprint/services/FormatterService.kt
@@ -10,9 +10,7 @@ import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.progress.BackgroundTaskQueue
 import com.intellij.openapi.progress.ProgressIndicator
-import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
-import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Ref
 import com.intellij.openapi.vfs.ReadonlyStatusHandler
