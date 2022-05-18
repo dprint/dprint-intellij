53c3762bb032b9adcb2477b24ee3739237dba2fd
Ryan Rushton <ryanr@canva.com>
2022-05-18 15:43:58 +1000

    Ensure project hasn't been disposed when sending messages.

--- src/main/kotlin/com/dprint/core/LogUtils.kt
+++ src/main/kotlin/com/dprint/core/LogUtils.kt
@@ -3,17 +3,18 @@ package com.dprint.core
 import com.dprint.messages.DprintMessage
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
+import com.intellij.util.messages.MessageBus
 
 object LogUtils {
     fun info(message: String, project: Project, logger: Logger) {
         logger.info(message)
-        project.messageBus.syncPublisher(DprintMessage.DPRINT_MESSAGE_TOPIC).info(message)
+        maybeGetMessageBus(project)?.syncPublisher(DprintMessage.DPRINT_MESSAGE_TOPIC)?.info(message)
     }
 
     fun warn(message: String, project: Project, logger: Logger) {
         // Always use info for system level logging as it throws notifications into the UI
         logger.info(message)
-        project.messageBus.syncPublisher(DprintMessage.DPRINT_MESSAGE_TOPIC).warn(message)
+        maybeGetMessageBus(project)?.syncPublisher(DprintMessage.DPRINT_MESSAGE_TOPIC)?.warn(message)
     }
 
     fun error(message: String, project: Project, logger: Logger) {
@@ -23,6 +24,14 @@ object LogUtils {
     fun error(message: String, throwable: Throwable?, project: Project, logger: Logger) {
         // Always use info for system level logging as it throws notifications into the UI
         logger.info(message, throwable)
-        project.messageBus.syncPublisher(DprintMessage.DPRINT_MESSAGE_TOPIC).error(message)
+        maybeGetMessageBus(project)?.syncPublisher(DprintMessage.DPRINT_MESSAGE_TOPIC)?.error(message)
+    }
+
+    private fun maybeGetMessageBus(project: Project): MessageBus? {
+        if (project.isDisposed) {
+            return null
+        }
+
+        return project.messageBus
     }
 }
