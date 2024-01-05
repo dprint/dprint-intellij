8e5a760f27ac0349f35ec2e127b5eeb60d3e3708
Ryan <ryan.rushton79@gmail.com>
2024-01-05 15:44:35 +1000

    More docs

--- src/main/kotlin/com/dprint/services/editorservice/FormatResult.kt
+++ src/main/kotlin/com/dprint/services/editorservice/FormatResult.kt
@@ -2,5 +2,7 @@ package com.dprint.services.editorservice
 
 /**
  * The resulting state of running the Dprint formatter.
+ *
+ * If both parameters are null, it represents a no-op from the format operation.
  */
 data class FormatResult(val formattedContent: String? = null, val error: String? = null)
