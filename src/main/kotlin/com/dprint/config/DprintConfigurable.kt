c476d54c07578db9d0b469dc6e41d7d1071ae52f
Ryan <ryan.rushton79@gmail.com>
2024-01-17 22:19:11 +1000

    Add run on save checkbox to action on save menu

--- src/main/kotlin/com/dprint/config/DprintConfigurable.kt
+++ src/main/kotlin/com/dprint/config/DprintConfigurable.kt
@@ -4,6 +4,10 @@ import com.dprint.i18n.DprintBundle
 import com.dprint.services.editorservice.EditorServiceManager
 import com.dprint.utils.validateConfigFile
 import com.dprint.utils.validateExecutablePath
+import com.intellij.ide.actionsOnSave.ActionOnSaveBackedByOwnConfigurable
+import com.intellij.ide.actionsOnSave.ActionOnSaveContext
+import com.intellij.ide.actionsOnSave.ActionOnSaveInfo
+import com.intellij.ide.actionsOnSave.ActionOnSaveInfoProvider
 import com.intellij.openapi.components.service
 import com.intellij.openapi.options.BoundSearchableConfigurable
 import com.intellij.openapi.project.Project
@@ -12,18 +16,21 @@ import com.intellij.ui.dsl.builder.LabelPosition
 import com.intellij.ui.dsl.builder.bindSelected
 import com.intellij.ui.dsl.builder.bindText
 import com.intellij.ui.dsl.builder.panel
+import javax.swing.JCheckBox
 
-private const val CONFIG_ID = "com.dprint.config"
+const val CONFIG_ID = "com.dprint.config"
 
 /**
  * Sets up the configuration panel for Dprint in the Tools section of preferences.
  */
-class ProjectConfigurable(private val project: Project) : BoundSearchableConfigurable(
+class DprintConfigurable(private val project: Project) : BoundSearchableConfigurable(
     DprintBundle.message("config.name"),
     "reference.settings.dprint",
     CONFIG_ID,
 ) {
-    @Suppress("LongMethod")
+    lateinit var enabledCheckBox: JCheckBox
+    lateinit var runOnSaveCheckbox: JCheckBox
+
     override fun createPanel(): DialogPanel {
         val projectConfig = project.service<ProjectConfiguration>()
         val userConfig = project.service<UserConfiguration>()
@@ -41,22 +48,26 @@ class ProjectConfigurable(private val project: Project) : BoundSearchableConfigu
 
             // Enabled checkbox
             row {
-                checkBox(DprintBundle.message("config.enable"))
-                    .bindSelected(
-                        { projectConfig.state.enabled },
-                        { projectConfig.state.enabled = it },
-                    )
-                    .comment(DprintBundle.message("config.enable.description"))
+                enabledCheckBox =
+                    checkBox(DprintBundle.message("config.enable"))
+                        .bindSelected(
+                            { projectConfig.state.enabled },
+                            { projectConfig.state.enabled = it },
+                        )
+                        .comment(DprintBundle.message("config.enable.description"))
+                        .component
             }
 
             // Format on save checkbox
             row {
-                checkBox(DprintBundle.message("config.run.on.save"))
-                    .bindSelected(
-                        { userConfig.state.runOnSave },
-                        { userConfig.state.runOnSave = it },
-                    )
-                    .comment(DprintBundle.message("config.run.on.save.description"))
+                runOnSaveCheckbox =
+                    checkBox(DprintBundle.message("config.run.on.save"))
+                        .bindSelected(
+                            { userConfig.state.runOnSave },
+                            { userConfig.state.runOnSave = it },
+                        )
+                        .comment(DprintBundle.message("config.run.on.save.description"))
+                        .component
             }
 
             // Default IJ override checkbox
@@ -125,6 +136,51 @@ class ProjectConfigurable(private val project: Project) : BoundSearchableConfigu
                     }.comment(DprintBundle.message("config.reload.description"))
                 }
             }
+        }
+    }
+
+    // This is used so that we can also have our "run on save" checkbox in the
+    // Tools -> Actions On Save menu
+    class DprintActionOnSaveInfoProvider : ActionOnSaveInfoProvider() {
+        override fun getActionOnSaveInfos(
+            actionOnSaveContext: ActionOnSaveContext,
+        ): MutableCollection<out ActionOnSaveInfo> {
+            return mutableListOf(DprintActionOnSaveInfo(actionOnSaveContext))
+        }
+
+        override fun getSearchableOptions(): MutableCollection<String> {
+            return mutableSetOf(DprintBundle.message("config.dprint.actions.on.save.run.dprint"))
+        }
+    }
+
+    private class DprintActionOnSaveInfo(actionOnSaveContext: ActionOnSaveContext) :
+        ActionOnSaveBackedByOwnConfigurable<DprintConfigurable>(
+            actionOnSaveContext,
+            CONFIG_ID,
+            DprintConfigurable::class.java,
+        ) {
+        override fun getActionOnSaveName() = DprintBundle.message("config.dprint.actions.on.save.run.dprint")
+
+        override fun isApplicableAccordingToStoredState(): Boolean {
+            return project.service<ProjectConfiguration>().state.enabled
+        }
+
+        override fun isApplicableAccordingToUiState(configurable: DprintConfigurable): Boolean {
+            return configurable.enabledCheckBox.isSelected
+        }
+
+        override fun isActionOnSaveEnabledAccordingToStoredState(): Boolean {
+            return project.service<UserConfiguration>().state.runOnSave
+        }
+
+        override fun isActionOnSaveEnabledAccordingToUiState(configurable: DprintConfigurable) =
+            configurable.runOnSaveCheckbox.isSelected
+
+        override fun setActionOnSaveEnabled(
+            configurable: DprintConfigurable,
+            enabled: Boolean,
+        ) {
+            configurable.runOnSaveCheckbox.isSelected = enabled
         }
     }
 }
